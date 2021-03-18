d5fc7882050ed2b4f50a4fa18813f5fdfc408076
layla  hammoud <laylahammoud63@gmail.com>
2021-03-18 16:25:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function map(src, cb) {
 
 function reduce(src, cb, starting){
   let r = (!!starting) ? starting : src[0]
-  let i = (!!starting) ? 0 : 1
+
 
   for (let i=0; i < src.length; i++) {
     r = cb(src[i], r)
